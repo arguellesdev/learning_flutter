a3670d2150092e9ff2bc1d625e7225fcb6fe402f
Anya <arguellesdev@gmail.com>
2024-01-17 10:47:14 -0700

    changes in files

--- andreabizzotto/collections/collection_m2.dart
+++ andreabizzotto/collections/collection_m2.dart
@@ -1,23 +1,23 @@
-import 'dart:io';
-
-void main(){
-  const pizzaPrices = {
-    'mexicana': 8,
-    'sonorense': 7,
-    'hawaiana': 5
-    };
-  const menu= ['mexicana', 'sonorense', 'hawaiana'];
-  var check =stdout.write('What do you want to order? ');
-  var personOrder= stdin.readLineSync();
-  var total = 0.0;
-  for (var item in menu){
-    final price = pizzaPrices[item];
-    if (menu.contains(personOrder) && (price != null)){
-      total += price;
-    break;
-    }else{
-      print('We don\'t have that pizza in our menu, check your order again');
-    }
-  }
-  print ('Check out total= ${total.toStringAsFixed(2)}');
-}
+// import 'dart:io';
+//
+// void main(){
+//   const pizzaPrices = {
+//     'mexicana': 8,
+//     'sonorense': 7,
+//     'hawaiana': 5
+//     };
+//   const menu= ['mexicana', 'sonorense', 'hawaiana'];
+//   var check =stdout.write('What do you want to order? ');
+//   var personOrder= stdin.readLineSync();
+//   var total = 0.0;
+//   for (var item in menu){
+//     final price = pizzaPrices[item];
+//     if (menu.contains(personOrder) && (price != null)){
+//       total += price;
+//     break;
+//     }else{
+//       print('We don\'t have that pizza in our menu, check your order again');
+//     }
+//   }
+//   print ('Check out total= ${total.toStringAsFixed(2)}');
+// }
--- andreabizzotto/data_processing/generics.dart
+++ andreabizzotto/data_processing/generics.dart
@@ -1,11 +1,17 @@
 void main(){
-  const list = [1 ,2, 3];
+  const list = [1 ,2, 3, 4];
   const list2 = [1.0 ,2.3, 3.5];
   list.forEach(print);
   final doubles = transform<int, int>(list, (x) => x * 2);
-  print (doubles);
+  print(doubles);
   final square = transform<double, int>(list2, (x) => x.round());
-  print (square);
+  print(square);
+  final even = list.where((value) => value % 2 == 0).toList();
+  print(even);
+  final val = list.firstWhere((x) => x == 4, orElse: () => -1 );
+  print(val);
+  final result = firstWhere(list, (x) => x == 3, orElse: () => -1);
+  print(result);
 }
 
 List<R> transform<T, R >(List<T> items, R Function(T) f){
@@ -14,4 +20,14 @@ List<R> transform<T, R >(List<T> items, R Function(T) f){
     result.add(f(x));
   }
   return result;
+}
+
+T firstWhere<T>(List<T> items, bool Function(T) f,
+    {required T Function() orElse}) {
+  for (var item in items) {
+    if (f(item)){
+      return item;
+    }
+  }
+  return orElse();
 }
