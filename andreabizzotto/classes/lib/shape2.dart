446871871735516e521a3f04fd46bd782a2c58c7
Anya <arguellesdev@gmail.com>
2024-01-19 10:53:57 -0700

    shape perimeter and area modificatios

--- andreabizzotto/classes/lib/shape2.dart
+++ andreabizzotto/classes/lib/shape2.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 abstract class Shape {
   double get area;
+  double get perimeter;
 }
 /*We can use abstract classes to define an interface that can be
 implemented by subclasses. Also decouples code that uses an interface from its
@@ -10,6 +11,7 @@ implementation. */
 
 void printArea(Shape shape) {
 print(shape.area);
+print(shape.perimeter);
   /* Dependency inversion Principle: code with abstraction,
   to be independent from specific implementations. print area() doesn't
   need to know that Square or Circle even exist only needs a Shape argument
@@ -22,10 +24,9 @@ class Square extends Shape {
 
   @override
   double get area => side * side;
-
-  @override
+  double get perimeter => 4 * side;
   String toString() {
-    return 'Square with side $side, has an Area of $area';
+    return 'Square with side $side, has an Area of $area and perimeter of $perimeter';
   }
 }
 
@@ -34,11 +35,10 @@ class Circle extends Shape {
   final double radius;
 
   @override
-  double get area => pi * radius * radius;
-
-  @override
+  double get area =>pi * radius * radius ;
+  double get perimeter => 2 * pi * radius ;
   String toString() {
-    return 'Circle with radius $radius, has an Area of $area';
+    return 'Circle with radius $radius, has a area of $area and perimeter of $perimeter';
   }
 }
 
